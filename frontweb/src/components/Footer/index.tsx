2987d148afa130fdbb11f30629b7fd71b16b1d6f
westorres9 <wester.torres9@gmail.com>
2022-06-06 16:32:19 -0300

    projeto melhorado

--- frontweb/src/components/Footer/index.tsx
+++ frontweb/src/components/Footer/index.tsx
@@ -1,20 +1,20 @@
 import './styles.css';
-import { ReactComponent as YoutubeIcon } from './youtube.svg'
+import { ReactComponent as Github } from './github.svg'
 import { ReactComponent as LinkedinIcon } from './linkedin.svg'
 import { ReactComponent as InstagramIcon } from './instagram.svg'
 
 function Footer() {
     return(
         <footer className="main-footer">
-            App desenvolvido por Wester Torres usando Java e Spring
+            App desenvolvido por Wester Torres
             <div className="footer-icons">
-                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
-                    <YoutubeIcon />
+                <a href="https://github.com/westorres9" target="_new">
+                    <Github />
                 </a>
-                <a href="https://www.linkedin.com/in/nathancarvalho/" target="_new">
+                <a href="https://www.linkedin.com/in/wester-torres-83a4b5219/" target="_new">
                     <LinkedinIcon />
                 </a>
-                <a href="https://www.instagram.com" target="_new">
+                <a href="https://www.instagram.com/westorres9" target="_new">
                     <InstagramIcon />
                 </a>
             </div>
